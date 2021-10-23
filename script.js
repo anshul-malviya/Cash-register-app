a200f40228e3b4ba16f767ef761fa5098344b56b
anshul malviya <malviya21anshul@gmail.com>
2021-10-23 11:07:38 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-var billAmount = document.querySelector("#bill-amount");
-var cashGiven = document.querySelector("#cash-given");
-var btn = document.querySelector("#btn-check");
-var input = billAmount.value;
+var billAmount = document.querySelector("#bill-amount")
+// var cashGiven = document.querySelector("#cash-given");
+// var btn = document.querySelector("#btn-check");
 
-btn.addEventListener("click", eventHandler);
-
-
-function eventHandler() {
-    cashGiven.innerText = "aaj";
-}
+// btn.addEventListener("click", function () {
+//     console.log(cashGiven.value);
+// })
+console.log(billAmount.value)
